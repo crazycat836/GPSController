a7529290a1b2eb7b9b85a75d7c7caf3e5c2379b3
Ming Chih Hsu <crazycat836@gmail.com>
2026-04-16 22:11:23 +0800

    chore: remove unused screenshots, add Windows launcher and PyInstaller spec

---
+++ GPSController.bat
@@ -0,0 +1,19 @@
+@echo off
+:: Check admin
+net session >nul 2>&1
+if %errorlevel% == 0 (
+    goto :RUN
+)
+
+:: Elevate using VBScript
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\gpscontroller_elevate.vbs"
+echo UAC.ShellExecute "%~f0", "", "%~dp0", "runas", 1 >> "%temp%\gpscontroller_elevate.vbs"
+cscript //nologo "%temp%\gpscontroller_elevate.vbs"
+del "%temp%\gpscontroller_elevate.vbs"
+exit /b
+
+:RUN
+cd /d "%~dp0"
+:: Use the full path to avoid the Windows Store python stub
+"C:\Users\USER\AppData\Local\Programs\Python\Python313\python.exe" start.py
+pause
